09ade986d38d1b8ac6f5904d4cab1588db370e42
Madhav12-4 <143866763+Madhav12-4@users.noreply.github.com>
2024-07-09 18:51:32 +0000

    variables and datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,21 @@
+const accountId = 144553
+let accountEmail = "abc@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+// accountId = 2
+
+
+accountEmail = "cba@google.com"
+accountPassword = "21212121"
+accountCity = "Bengaluru"
+let accountState;
+console.table([accountEmail,accountPassword,accountCity,accountState])
+
+/* 
+Prefer Not to use var
+because of issue  in block scope and functional scope
+ */
+
+
+console.log(accountId);
