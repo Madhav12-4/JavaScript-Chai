c378a0ba27549264abf0f182870ca8188a5053d4
Madhav12-4 <143866763+Madhav12-4@users.noreply.github.com>
2024-07-10 04:37:09 +0000

    Conversion of Datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,29 @@
+// let score = undefined
+
+// console.log(typeof score);
+// console.log(typeof( score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+// Null => 0
+//undefined => NaN
+
+let isLoggedIn = "Madhav"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true ; 0 => false
+// "" => false
+// "Madhav" => true
+
+let someNumber = 33
+
+letstringNumber = String(someNumber)
+console.log(letstringNumber);
+console.log(typeof letstringNumber);
