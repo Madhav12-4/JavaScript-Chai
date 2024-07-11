fa484a9b6a88202cf0370f6319a077e714e1bfb3
Madhav12-4 <143866763+Madhav12-4@users.noreply.github.com>
2024-07-11 07:16:36 +0000

    arrays part 2

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -35,4 +35,41 @@ function loginUserMessage(userName)
 }
 
 // console.log(loginUserMessage("Madhav_Narayan"));
-console.log(loginUserMessage());
+// console.log(loginUserMessage());
+
+
+function calculateCartPrice(val1,val2,...num1)
+{
+    return num1
+}
+
+console.log(calculateCartPrice(200,400,500));
+
+
+const user = {
+    userName: "Madhav",
+    price: 199
+}
+
+function handleObject(anyObject)
+{
+    console.log(`Username is ${anyObject.userName} and price is ${anyObject.price}`);
+}
+
+// handleObject(user)
+handleObject({
+    userName: "Sam",
+    price : 399
+})
+
+
+const myNewArray = [200,400,100,600]
+
+function returnSecondValue(getArray)
+{
+    return getArray[1]
+}
+
+console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([4327396,3424,424,243]));
+
